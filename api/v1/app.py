28cc5d355d79494c948b1a81fe80a5e7de9d62b5
Darius Tohtin <dariustohtin@gmail.com>
2024-04-27 05:54:50 -0700

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """My Flask restful api first endpoint"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def close_DBsession(exception):
     """close DB session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    """Handle 404 error"""
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == "__main__":
