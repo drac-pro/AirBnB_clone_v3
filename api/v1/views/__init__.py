53acc761fa835421384c74ac7e0f64c53ff3d30b
Darius Tohtin <dariustohtin@gmail.com>
2024-04-27 04:11:52 -0700

    3. Status of your API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Initialize the views model"""
+"""Initialize the views model for the Restful API"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,7 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Restful API status"""
-    return jsonify({"ststus": "OK"})
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
